99dec25c21ed110042fbddae55ca75494de1095f
Nnamdi <nnamdiibns@gmail.com>
2017-09-14 14:14:22 +0100

    Remove useless tests

--- test.py
+++ test.py
@@ -1,29 +1,3 @@
 from linkedlist import LinkedList
 from node import Node
 
-iterable = ("my", "name", "is", "nnamdi")
-l = LinkedList(iterable)
-
-l.inserttail("Ibeanusi")
-c = None
-for n in l:
-	if n.data == "name":
-		c=n
-		break
-
-
-i = Node("tags")
-j = Node("Full")
-l.postinsert(n, i )
-
-for n in l:
-	print(n.data)
-
-h = l.getnode(2)
-l.preinsert(5, j)
-print("*"*10)
-
-for n in l:
-	print(n.data)
-
-
