102634daa1a3cd314aa2f8a00d3079a0ed61f0e8
Nnamdi <nnamdiibns@gmail.com>
2017-09-14 19:46:17 +0100

    Create skeleton for tests

--- test.py
+++ test.py
@@ -1,3 +1,44 @@
 from linkedlist import LinkedList
 from node import Node
 
+iterable = ('my', 'name', 'is', 'nnamdi')
+class TestLinkedList:
+  singly = LinkedList(iterable)
+  doubly = LinkedList(iterable, double=True)
+
+  def test_init():
+    pass
+
+  def test_iter():
+    pass
+
+  def test_len():
+    pass
+
+  def test_eq():
+    pass
+
+  def test_bool():
+    pass
+
+  def test_isempty():
+    pass
+
+  def test_inserthead():
+    pass
+
+  def test_inserttail():
+    pass
+
+  def test_preinsert():
+    pass
+
+  def test_postinsert():
+    pass
+
+  def test_iscycle():
+    pass
+
+  def test_getnode():
+    pass
+    
