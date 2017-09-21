5319845e591e0d2fd75f0fc2d03bdf9763fd4de8
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2017-09-21 22:12:43 +0100

    Create new BNode class for BTree nodes

--- node.py
+++ node.py
@@ -4,9 +4,6 @@
 class LNode:
   next = None
   prev = None
-  left = None
-  right = None
-  parent = None
   data = None
 
   def __init__(self, data=None, next=None, prev=None):
@@ -27,5 +24,25 @@ class LNode:
       equal = equal and (self.data is other.data)
     return equal
 
+
+class BNode:
+  left = None
+  right = None
+  parent = None
+  data = None
+
+  def __init__(self, data=None, left=None, right=None, parent=None):
+    if data:
+      self.data = data
+    if left:
+      self.left = left
+    if right:
+      self.right = right
+    if parent:
+      self.parent = parent
+
   def isleaf(self):
     return not(self.left or self.right)
+
+  def isroot(self):
+    return not self.parent
