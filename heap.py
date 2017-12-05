c0ba5f097f89bf4eefcfebcbdd8ec487b5ca8d0c
Nnamdi <nnamdiibns@gmail.com>
2017-12-05 23:05:29 +0100

    Create skeleton for Heap data structure

---
+++ heap.py
@@ -0,0 +1,26 @@
+from treenode.node import Node
+
+class Heap:
+  root = None
+  size = 0
+
+  def __init__(self):
+    pass
+
+  def __len__(self):
+    return self.size
+
+  def extract(self):
+    return self.root
+
+  def insert(self, item):
+    pass
+
+  def bubble_up(self, item):
+    pass
+
+  def delete(self, item):
+    pass
+
+  def buble_down(self, item):
+    pass
